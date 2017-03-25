bf99c3c976f01569075f5fb4d7b9b80029474b6c
Михаил Абросимов <michael.abrosimov@gillsoft.tech>
2017-03-25 22:31:34 +0200

    Форма ввода контактных данных

---
+++ CreateRecord.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ Задание №1
+ Создать форму, заполнения данными
+ */
+
+echo ('Заполните контактные данные.');
+?>
+<form>
+    <p> Фамилия: <input type="text" name="second_name" /></p>
+    <p> Имя: <input type="text" name="first_name" /></p>
+    <p> Дата рождения: <input type="date" name="birth_date" /></p>
+    <p> e-Mail: <input type="email" name="e_mail" /></p>
+    <p> <input type="submit" /><input type="reset" /></p>
+</form>
---
+++ Index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael-mac
+ * Date: 25.03.17
+ * Time: 21:38
+ */
+echo ('Моя программа');
